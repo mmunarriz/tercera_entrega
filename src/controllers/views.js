fc4c4b82f1899628988a05a8012d516cecd50e33
Mariano Munarriz <mmunarriz@gmail.com>
2023-10-30 11:17:39 -0300

    add views controllers

---
+++ src/controllers/views.js
@@ -0,0 +1,103 @@
+import { cookieExtractor } from '../utils.js';
+import productsModel from '../dao/models/products.js';
+import requireAuth from './auth.js';
+
+
+// Controlador para la vista de inicio
+export function getHomePage(req, res) {
+    res.render('login');
+}
+
+// Controlador para la vista de inicio de sesión
+export function getLoginPage(req, res) {
+    const token = req.cookies.access_token;
+    // Verificar si el usuario ya está logueado
+    if (token) {
+        return res.redirect('/products'); // Redireccionar a "/products" si ya está logueado
+    }
+    res.render('login');
+}
+
+// Controlador para la vista de registro
+export function getRegisterPage(req, res) {
+    const token = req.cookies.access_token;
+    // Verificar si el usuario ya está logueado
+    if (token) {
+        return res.redirect('/products'); // Redireccionar a "/products" si ya está logueado
+    }
+    res.render('register');
+}
+
+// Controlador para la vista de perfil
+export function getProfilePage(req, res) {
+    // Verifica la autenticación
+    requireAuth(req, res, () => {
+        // Recupera el token de la cookie "access_token"
+        const user = cookieExtractor(req);
+
+        if (!user) {
+            return res.status(401).json({ status: "error", message: "Token not provided" });
+        }
+        const { name, email, role } = user;
+        res.render('profile', { user });
+    });
+}
+
+// Controlador para la vista actual
+export function getCurrentPage(req, res) {
+    // Verifica la autenticación
+    requireAuth(req, res, () => {
+        // Recupera el token de la cookie "access_token"
+        const user = cookieExtractor(req);
+
+        if (!user) {
+            return res.status(401).json({ status: "error", message: "Token not provided" });
+        }
+        const { name, email, role } = user;
+        res.render('current', { user });
+    });
+}
+
+// Controlador para la vista de productos
+export async function getProductsPage(req, res) {
+    // Verifica la autenticación
+    requireAuth(req, res, async () => {
+        try {
+            const { page = 1 } = req.query;
+            const result = await productsModel.paginate({}, { limit: 8, page, lean: true });
+            const { docs, hasPrevPage, hasNextPage, nextPage, prevPage } = result;
+            const products = docs;
+
+            // Recupera el token de la cookie "access_token"
+            const user = cookieExtractor(req);
+
+            if (!user) {
+                return res.status(401).json({ status: "error", message: "Token not provided" });
+            }
+            const { name, email, role } = user;
+
+            res.render('products', {
+                user,
+                products,
+                hasPrevPage,
+                hasNextPage,
+                prevPage,
+                nextPage
+            });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ status: "error", message: "Internal Server Error" });
+        }
+    });
+}
+
+// Controlador para la vista de cierre de sesión
+export function logoutUser(req, res) {
+    // Verifica la autenticación
+    requireAuth(req, res, () => {
+        return res
+            .clearCookie("access_token")
+            .status(200)
+            .redirect("/login"); // Redireccionar al usuario a la vista de login
+    });
+}
--- src/routes/views.js
+++ src/routes/views.js
@@ -1,90 +1,19 @@
 import { Router } from 'express';
-import { cookieExtractor } from '../utils.js'
-import productsModel from "../dao/models/products.js";
-import requireAuth from "../controllers/auth.js"
+import { getHomePage, getLoginPage, getRegisterPage, getProfilePage, getCurrentPage, getProductsPage, logoutUser } from '../controllers/views.js';
+
 
 const router = Router();
 
-// Ruta de acceso libre
-router.get('/', (req, res) => {
-    res.render('login');
-})
+// Rutas de acceso
+router.get('/', getHomePage);
+router.get('/login', getLoginPage);
+router.get('/register', getRegisterPage);
 
-// Ruta protegida: (solo accesible si el usuario no está autenticado)
-router.get('/login', (req, res) => {
-    const token = req.cookies.access_token;
-    // Verificar si el usuario ya está logueado
-    if (token) {
-        return res.redirect('/products'); // Redireccionar a "/products" si ya está logueado
-    }
-    // Si no tiene una cookie vigente, permite el acceso a la vista de login
-    res.render('login');
-});
+// Rutas que requieren estar autenticado
+router.get('/profile', getProfilePage);
+router.get('/current', getCurrentPage);
+router.get('/products', getProductsPage);
+router.get('/logout', logoutUser);
 
-// Ruta protegida: (solo accesible si el usuario no está autenticado)
-router.get('/register', (req, res) => {
-    const token = req.cookies.access_token;
-    // Verificar si el usuario ya está logueado
-    if (token) {
-        return res.redirect('/products'); // Redireccionar a "/products" si ya está logueado
-    }
-    // Si no tiene una cookie vigente, permite el acceso a la vista de login
-    res.render('register');
-});
-
-// Ruta requiere estar autenticado
-router.get('/profile', requireAuth, (req, res) => {
-    // Recupera el token de la cookie "access_token"
-    const user = cookieExtractor(req);
-
-    if (!user) {
-        return res.status(401).json({ status: "error", message: "Token not provided" });
-    }
-    const { name, email, role } = user;
-    res.render('profile', { user });
-})
-
-// Ruta requiere estar autenticado
-router.get('/current', requireAuth, (req, res) => {
-    // Recupera el token de la cookie "access_token"
-    const user = cookieExtractor(req);
-
-    if (!user) {
-        return res.status(401).json({ status: "error", message: "Token not provided" });
-    }
-    const { name, email, role } = user;
-    res.render('current', { user });
-})
-
-router.get("/logout", requireAuth, (req, res) => {
-    return res
-        .clearCookie("access_token")
-        .status(200)
-        .redirect("/login"); // Redireccionar al usuario a la vista de login
-});
-
-// Ruta requiere estar autenticado
-router.get('/products', requireAuth, async (req, res) => {
-    const { page = 1 } = req.query;
-    const { docs, hasPrevPage, hasNextPage, nextPage, prevPage } = await productsModel.paginate({}, { limit: 8, page, lean: true });
-    const products = docs;
-
-    // Recupera el token de la cookie "access_token"
-    const user = cookieExtractor(req);
-
-    if (!user) {
-        return res.status(401).json({ status: "error", message: "Token not provided" });
-    }
-    const { name, email, role } = user;
-
-    res.render('products', {
-        user,
-        products,
-        hasPrevPage,
-        hasNextPage,
-        prevPage,
-        nextPage
-    });
-})
 
 export default router;
